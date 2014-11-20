083cb2e0eb1218777efc862ce29f360839952859
Kelvin Morais <morais.kelvin@gmail.com>
2014-11-20 21:51:17 -0200

    created redirect

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   namespace :api, path: '/', constraints: { subdomain: 'api' } do
     resources :posts, only: [:index, :show, :create]
   end
+
+	get '/', to: redirect('/posts')
 end
