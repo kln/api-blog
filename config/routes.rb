3d645e9174fb921b0b15f5647e63e502a2daf82b
Kelvin Morais <morais.kelvin@gmail.com>
2014-11-21 18:45:53 -0200

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, path: '/', constraints: { subdomain: 'api' } do
+  namespace :api, path: '/' do
     resources :posts, only: [:index, :show, :create]
   end
 
